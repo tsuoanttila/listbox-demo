244ef58cedaac882b23f437adbb659b72c2ae28a
Teemu Suo-Anttila <teemusa@vaadin.com>
2015-11-27 22:32:10 +0200

    Fire events from the component when selection changes

--- src/main/java/org/vaadin/teemusa/listbox/ListBox.java
+++ src/main/java/org/vaadin/teemusa/listbox/ListBox.java
@@ -1,18 +1,21 @@
 package org.vaadin.teemusa.listbox;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
 
 import org.vaadin.teemusa.TypedComponent;
 import org.vaadin.teemusa.beandatasource.CollectionDataSource;
 import org.vaadin.teemusa.beandatasource.DataProvider;
 import org.vaadin.teemusa.beandatasource.interfaces.DataGenerator;
 import org.vaadin.teemusa.client.listbox.ListBoxSelectRpc;
+import org.vaadin.teemusa.listbox.ValueChange.ValueChangeListener;
+import org.vaadin.teemusa.listbox.ValueChange.ValueChangeNotifier;
 
 import com.vaadin.ui.AbstractComponent;
 
 import elemental.json.JsonObject;
 
-public class ListBox<T> extends AbstractComponent implements TypedComponent<T> {
+public class ListBox<T> extends AbstractComponent implements TypedComponent<T>, ValueChangeNotifier<T> {
 
 	public interface NameProvider<T> {
 		String getName(T value);
@@ -20,6 +23,7 @@ public class ListBox<T> extends AbstractComponent implements TypedComponent<T> {
 
 	private DataProvider<T> dataProvider;
 	private T selected;
+	private Collection<ValueChangeListener<T>> listeners = new LinkedHashSet<ValueChangeListener<T>>();
 
 	public ListBox(Collection<T> values, final NameProvider<T> nameProvider) {
 		super();
@@ -44,9 +48,25 @@ public class ListBox<T> extends AbstractComponent implements TypedComponent<T> {
 				T oldValue = selected;
 				selected = dataProvider.getKeyMapper().get(key);
 				if (oldValue != selected) {
-					// TODO: fire event
+					// TODO: use fireEvent
+					ValueChange<T> event = new ValueChange<T>(oldValue, selected);
+					for (ValueChangeListener<T> listener : listeners) {
+						listener.valueChange(event);
+					}
 				}
 			}
 		});
+	}
+
+	@Override
+	public void addValueChangeListener(ValueChangeListener<T> listener) {
+		// TODO: use proper event bus
+		listeners.add(listener);
+	}
+
+	@Override
+	public void removeValueChangeListener(ValueChangeListener<T> listener) {
+		// TODO: use proper event bus
+		listeners.remove(listener);
 	}
 }
