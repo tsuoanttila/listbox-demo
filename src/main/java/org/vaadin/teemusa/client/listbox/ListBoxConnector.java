e31ecc56bf0ed75ac3a40b6cfcca7019691dff66
Teemu Suo-Anttila <teemusa@vaadin.com>
2015-11-27 21:38:03 +0200

    Change client-side widget to GWT ListBox

--- src/main/java/org/vaadin/teemusa/client/listbox/ListBoxConnector.java
+++ src/main/java/org/vaadin/teemusa/client/listbox/ListBoxConnector.java
@@ -1,16 +1,21 @@
 package org.vaadin.teemusa.client.listbox;
 
 import org.vaadin.teemusa.beandatasource.client.HasDataSource;
-import org.vaadin.teemusa.listbox.ListBox;
 
+import com.google.gwt.user.client.ui.ListBox;
 import com.vaadin.client.data.DataSource;
 import com.vaadin.client.ui.AbstractComponentConnector;
 import com.vaadin.shared.ui.Connect;
 
 import elemental.json.JsonObject;
 
-@Connect(ListBox.class)
+@Connect(org.vaadin.teemusa.listbox.ListBox.class)
 public class ListBoxConnector extends AbstractComponentConnector implements HasDataSource {
+
+	@Override
+	public ListBox getWidget() {
+		return (ListBox) super.getWidget();
+	}
 
 	@Override
 	public void setDataSource(DataSource<JsonObject> rpcDataSource) {
