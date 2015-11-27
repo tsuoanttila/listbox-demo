487fc3970c33a6c3aad9d6bb213f2b71b0c7c17b
Teemu Suo-Anttila <teemusa@vaadin.com>
2015-11-27 21:42:08 +0200

    Handle setting the data source

--- src/main/java/org/vaadin/teemusa/client/listbox/ListBoxConnector.java
+++ src/main/java/org/vaadin/teemusa/client/listbox/ListBoxConnector.java
@@ -3,6 +3,7 @@ package org.vaadin.teemusa.client.listbox;
 import org.vaadin.teemusa.beandatasource.client.HasDataSource;
 
 import com.google.gwt.user.client.ui.ListBox;
+import com.vaadin.client.data.DataChangeHandler;
 import com.vaadin.client.data.DataSource;
 import com.vaadin.client.ui.AbstractComponentConnector;
 import com.vaadin.shared.ui.Connect;
@@ -12,6 +13,8 @@ import elemental.json.JsonObject;
 @Connect(org.vaadin.teemusa.listbox.ListBox.class)
 public class ListBoxConnector extends AbstractComponentConnector implements HasDataSource {
 
+	private DataSource<JsonObject> dataSource;
+
 	@Override
 	public ListBox getWidget() {
 		return (ListBox) super.getWidget();
@@ -19,6 +22,42 @@ public class ListBoxConnector extends AbstractComponentConnector implements HasD
 
 	@Override
 	public void setDataSource(DataSource<JsonObject> rpcDataSource) {
-		// TODO Auto-generated method stub
+		// Clean up old data source
+		if (dataSource != null) {
+			dataSource.setDataChangeHandler(null);
+		}
+
+		dataSource = rpcDataSource;
+		dataSource.setDataChangeHandler(new DataChangeHandler() {
+
+			@Override
+			public void resetDataAndSize(int estimatedNewDataSize) {
+				resetValues();
+			}
+
+			@Override
+			public void dataUpdated(int firstRowIndex, int numberOfRows) {
+				resetValues();
+			}
+
+			@Override
+			public void dataRemoved(int firstRowIndex, int numberOfRows) {
+				resetValues();
+			}
+
+			@Override
+			public void dataAvailable(int firstRowIndex, int numberOfRows) {
+				resetValues();
+			}
+
+			@Override
+			public void dataAdded(int firstRowIndex, int numberOfRows) {
+				resetValues();
+			}
+		});
+	}
+
+	private void resetValues() {
+		// Here we're supposed to reset all the data in the ListBox widget
 	}
 }
