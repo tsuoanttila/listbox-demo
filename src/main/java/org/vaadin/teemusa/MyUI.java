8f70723f91b9c525a0b57a74d9506536b1f617fc
Teemu Suo-Anttila <teemusa@vaadin.com>
2015-11-27 22:39:10 +0200

    Create Reservation bean

--- src/main/java/org/vaadin/teemusa/MyUI.java
+++ src/main/java/org/vaadin/teemusa/MyUI.java
@@ -1,5 +1,10 @@
 package org.vaadin.teemusa;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
@@ -13,32 +18,55 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-/**
- *
- */
 @Theme("mytheme")
 @Widgetset("org.vaadin.teemusa.MyAppWidgetset")
 public class MyUI extends UI {
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
 
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
+		Button button = new Button("Click Me");
+		button.addClickListener(new Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				layout.addComponent(new Label("Thank you for clicking"));
+			}
+		});
+		layout.addComponent(button);
 
-    }
+	}
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
+}
+
+class Reservation {
+
+	private String reservationName;
+
+	public Reservation(String reservationName) {
+		this.setReservationName(reservationName);
+	}
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
+
+	public static Collection<Reservation> generateReservations() {
+		List<String> names = Arrays.asList("Teemu", "Henri", "Johannes", "Sami", "Anssi", "Markus", "Ilya");
+		List<Reservation> reservations = new ArrayList<Reservation>();
+		for (String name : names) {
+			reservations.add(new Reservation(name));
+		}
+		return reservations;
+	}
 }
